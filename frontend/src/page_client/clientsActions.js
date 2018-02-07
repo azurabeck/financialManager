02b5ac2a372f88b42d908ac4738de7964e21fc3d
Rebecca <rebecca@psafe.com>
2018-02-07 15:38:12 -0200

    erro undefined

--- frontend/src/page_client/clientsActions.js
+++ frontend/src/page_client/clientsActions.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:3003/api/clientCycle'
+const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}`) 
+    const request = axios.get(`${BASE_URL}/clientCycle`) 
     return {
         type: 'CLIENT_LIST_FETCHED',
         payload: request
