e90ba7b71cdcecf2bba66478723655df36d198b7
Rebecca <rebecca@psafe.com>
2018-02-08 21:16:45 -0200

    adicionando funções backend para autenticação

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,37 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server) {
+
+module.exports = function (server) {
 
     // Definir URL base para todas as rotas 
     const router = express.Router()
-    server.use('/api', router)
+    server.use('/api', router) 
 
-    // Rotas de Ciclo de Pagamento 
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
+
+    // Rotas da Lista Cliente 
     const clientCycle = require('../api/clientCycle/clientCycleService')
     clientCycle.register(router, '/clientCycle')
+
+    /* Rotas da Login
+    const clientCycle = require('../api/clientCycle/clientCycleService')
+    clientCycle.register(protectedApi, '/clientCycle')*/
+
+    /*
+      * Rotas abertas
+      */
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
