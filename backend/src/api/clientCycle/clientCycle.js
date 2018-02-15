d0c8615ee653558abf7003ee797142694e7211eb
Rebecca <rebecca@psafe.com>
2018-02-15 15:26:37 -0200

    alteração do backend

--- backend/src/api/clientCycle/clientCycle.js
+++ backend/src/api/clientCycle/clientCycle.js
@@ -1,10 +1,15 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
+const emailsSchema = new mongoose.Schema({
+    email: { type: String, required: true }
+})
+
 const clientSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    cpf: { type: Number, required: true },
-    emails: { type: String, required: true }
+    cpf: { type: Number, required: true },    
+    notes: { type: Number, required: false },
+    emails: [emailsSchema]
 })
 
 module.exports = restful.model('BillingCycle', clientSchema)
