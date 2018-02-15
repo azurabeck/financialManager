991a6254e528f57822bb603a1329ba77c4d398b7
Rebecca <rebecca@psafe.com>
2018-02-15 20:12:43 -0200

    fixing error: exibir o conteudo dos menus

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -3,9 +3,9 @@ import MenuItem from './menuItem'
 
 export default props => (
     <ul className='sidebar-menu'>
-        <MenuItem path='#/clientes' label='Clientes' icon='user-o'/>           
-        <MenuItem path='#/emails' label='Enviar Emails' icon='comment-o'/>
-        <MenuItem path='#/enviados' label='Emails Enviados' icon='envelope-open-o' />
-        <MenuItem path='#/controle' label='Controle' icon='ravelry' />
+        <MenuItem path='clientes' label='Clientes' icon='user-o'/>           
+        <MenuItem path='emails' label='Enviar Emails' icon='comment-o'/>
+        <MenuItem path='enviados' label='Emails Enviados' icon='envelope-open-o' />
+        <MenuItem path='controle' label='Controle' icon='ravelry' />
     </ul>
 )
